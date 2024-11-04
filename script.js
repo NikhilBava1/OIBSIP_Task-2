0aa381ca6ef90579ddf0f28b8cc15965841706c6
nikhilbava1 <nikhilbawa172@gmail.com>
2024-11-04 18:51:02 +0530

    first commit

--- script.js
+++ script.js
@@ -21,6 +21,10 @@ toggleButtons.forEach(button => {
     });
 });
 const contactLink = document.querySelector('.contact-link');
+const aboutLink = document.querySelector('.about-link');
+const serviceLink = document.querySelector('.service-links');
+const about = document.querySelector('.about-me');
+const service = document.querySelector('.footer-section.service-links');
 const footerContact = document.querySelector('.footer-section.contact-info');
 
 contactLink.addEventListener('click', (event) => {
@@ -37,6 +41,34 @@ contactLink.addEventListener('click', (event) => {
         footerContact.classList.remove('show-box-shadow');
     }, 3000); // 3000 milliseconds = 3 seconds
 });
+aboutLink.addEventListener('click', (event) => {
+    event.preventDefault(); // Prevent default anchor behavior
+
+    // Smooth scroll to footer
+    about.scrollIntoView({
+        behavior: 'smooth'
+    });
+
+    // Add box shadow to footer contact box for 2-3 seconds
+    about.classList.add('show-box-shadow');
+    setTimeout(() => {
+        about.classList.remove('show-box-shadow');
+    }, 3000); // 3000 milliseconds = 3 seconds
+});
+serviceLink.addEventListener('click', (event) => {
+    event.preventDefault(); // Prevent default anchor behavior
+
+    // Smooth scroll to footer
+    service.scrollIntoView({
+        behavior: 'smooth'
+    });
+
+    // Add box shadow to footer contact box for 2-3 seconds
+    service.classList.add('show-box-shadow');
+    setTimeout(() => {
+        service.classList.remove('show-box-shadow');
+    }, 3000); // 3000 milliseconds = 3 seconds
+});
 
 document.addEventListener("DOMContentLoaded", () => {
     const progressBars = document.querySelectorAll(".progress-bar");
@@ -45,4 +77,12 @@ document.addEventListener("DOMContentLoaded", () => {
         const skillLevel = bar.getAttribute("data-skill");
         bar.style.width = skillLevel;
     });
+});
+
+
+document.getElementById("dwnldbtn").addEventListener("click", function() {
+    const link = document.createElement("a");
+    link.href = "Resume.pdf";
+    link.download = "Nikhil_CV.pdf";
+    link.click();
 });
